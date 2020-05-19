bc11d15698efeba3f2776a4e5aeba07f82c4c044
Daniel Ioannou <dioann28@gmail.com>
2020-05-19 14:32:47 +0300

    initialize example

---
+++ example/android/app/src/main/kotlin/com/adaptiveActionSheet/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.adaptiveActionSheet.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Adaptive action sheet Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Adaptive action sheet example'),
+      ),
+      body: Center(
+        child: Text(
+          'You have pushed the button this many times:',
+        ),
+      ),
+    );
+  }
+}
