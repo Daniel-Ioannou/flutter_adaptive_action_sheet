128978ab65ce927c403217e3e0fb93f867b1ff80
-Daniel Ioannou <dioann28@gmail.com>
2021-03-08 19:49:10 +0200

    #19: Migrate to null safety

--- lib/src/cancel_action.dart
+++ lib/src/cancel_action.dart
@@ -13,13 +13,13 @@ class CancelAction {
 
   /// The callback that is called when the action item is tapped.
   /// [onPressed] is optional by default will dismiss the Action Sheet.
-  final VoidCallback onPressed;
+  final VoidCallback? onPressed;
 
   /// The TextStyle to use for the title text. (optional)
-  final TextStyle textStyle;
+  final TextStyle? textStyle;
 
   CancelAction({
-    @required this.title,
+    required this.title,
     this.onPressed,
     this.textStyle,
   });
